2b3e65da344a9bebbdbf87e805be6071988a7c46
Eric Kim <kim2000@uw.edu>
2020-04-17 19:38:44 +0900

    init

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php include 'layout/header.php'; ?>
+
+  <ul>
+    <li>
+      <a href="create.php"><strong>Create</strong></a> - add a user
+    </li>
+    <li>
+      <a href="read.php"><strong>Read</strong></a> - find a user
+    </li>
+  </ul>
+
+<?php include 'layout/footer.php'; ?>
